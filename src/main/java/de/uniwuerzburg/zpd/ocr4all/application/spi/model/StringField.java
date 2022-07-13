1d6ae8a0cef9d64c55fbdeebc232733dbd8e8134
Herbert Baier <baier@itbaier.de>
2022-07-13 11:44:36 +0200

    spi/added content-type to string field

--- src/main/java/de/uniwuerzburg/zpd/ocr4all/application/spi/model/StringField.java
+++ src/main/java/de/uniwuerzburg/zpd/ocr4all/application/spi/model/StringField.java
@@ -7,6 +7,8 @@
  */
 package de.uniwuerzburg.zpd.ocr4all.application.spi.model;
 
+import java.util.Optional;
+
 import de.uniwuerzburg.zpd.ocr4all.application.spi.env.Internationalization;
 
 /**
@@ -17,6 +19,10 @@ import de.uniwuerzburg.zpd.ocr4all.application.spi.env.Internationalization;
  * @since 1.8
  */
 public final class StringField extends Field<String> {
+	/**
+	 * The content type.
+	 */
+	private final Optional<String> contentType;
 
 	/**
 	 * Creates a string field for a model.
@@ -31,7 +37,7 @@ public final class StringField extends Field<String> {
 	 */
 	public StringField(String argument, String value, Internationalization description,
 			Internationalization placeholder) throws IllegalArgumentException {
-		super(argument, value, description, null, placeholder, false);
+		this(argument, value, description, null, placeholder, false);
 	}
 
 	/**
@@ -49,7 +55,39 @@ public final class StringField extends Field<String> {
 	 */
 	public StringField(String argument, String value, Internationalization description, Internationalization warning,
 			Internationalization placeholder, boolean isDisabled) throws IllegalArgumentException {
+		this(argument, value, null, description, warning, placeholder, isDisabled);
+	}
+
+	/**
+	 * Creates a string field for a model.
+	 * 
+	 * @param argument    The argument.
+	 * @param value       The default value. Null if not required.
+	 * @param contentType The content type. Null if not required.
+	 * @param description The description.
+	 * @param warning     The warning. Null if no warning is required.
+	 * @param placeholder The placeholder. Null if no placeholder is required.
+	 * @param isDisabled  True if the field is disabled.
+	 * @throws IllegalArgumentException Throws if the argument or the description is
+	 *                                  null.
+	 * @since 1.8
+	 */
+	public StringField(String argument, String value, String contentType, Internationalization description,
+			Internationalization warning, Internationalization placeholder, boolean isDisabled)
+			throws IllegalArgumentException {
 		super(argument, value, description, warning, placeholder, isDisabled);
+
+		this.contentType = contentType == null ? Optional.empty() : Optional.of(contentType);
+	}
+
+	/**
+	 * Returns the content type.
+	 *
+	 * @return The content type.
+	 * @since 1.8
+	 */
+	public Optional<String> getContentType() {
+		return contentType;
 	}
 
 }
