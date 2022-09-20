dded48106a587427a113e2d7b43995972795ad49
Herbert Baier <baier@itbaier.de>
2022-09-20 17:10:16 +0200

    postcorrection/added service provider

---
+++ src/main/java/de/uniwuerzburg/zpd/ocr4all/application/spi/PostcorrectionServiceProvider.java
@@ -0,0 +1,21 @@
+/**
+ * File:     PostcorrectionServiceProvider.java
+ * Package:  de.uniwuerzburg.zpd.ocr4all.application.spi
+ * 
+ * Author:   Herbert Baier (herbert.baier@uni-wuerzburg.de)
+ * Date:     20.09.2022
+ */
+package de.uniwuerzburg.zpd.ocr4all.application.spi;
+
+import de.uniwuerzburg.zpd.ocr4all.application.spi.core.ProcessServiceProvider;
+
+/**
+ * Defines service provider interfaces for post-correction.
+ *
+ * @author <a href="mailto:herbert.baier@uni-wuerzburg.de">Herbert Baier</a>
+ * @version 1.0
+ * @since 1.8
+ */
+public interface PostcorrectionServiceProvider extends ProcessServiceProvider {
+
+}
