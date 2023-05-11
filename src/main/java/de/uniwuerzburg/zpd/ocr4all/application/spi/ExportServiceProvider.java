1c266a5533a8c32bbce21d322f768873bd397879
Herbert Baier <baier@itbaier.de>
2023-05-11 12:43:14 +0200

    spi/added type tool and export

---
+++ src/main/java/de/uniwuerzburg/zpd/ocr4all/application/spi/ExportServiceProvider.java
@@ -0,0 +1,21 @@
+/**
+ * File:     ExportServiceProvider.java
+ * Package:  de.uniwuerzburg.zpd.ocr4all.application.spi
+ * 
+ * Author:   Herbert Baier (herbert.baier@uni-wuerzburg.de)
+ * Date:     11.05.2023
+ */
+package de.uniwuerzburg.zpd.ocr4all.application.spi;
+
+import de.uniwuerzburg.zpd.ocr4all.application.spi.core.ProcessServiceProvider;
+
+/**
+ * Defines service provider interfaces for exports.
+ *
+ * @author <a href="mailto:herbert.baier@uni-wuerzburg.de">Herbert Baier</a>
+ * @version 1.0
+ * @since 1.8
+ */
+public interface ExportServiceProvider extends ProcessServiceProvider {
+
+}
