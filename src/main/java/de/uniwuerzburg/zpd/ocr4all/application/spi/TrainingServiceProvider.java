46d78012c62c57ab3009dd77fb81782411944aa2
Herbert Baier <baier@itbaier.de>
2024-06-20 17:52:11 +0200

    service provider/training

---
+++ src/main/java/de/uniwuerzburg/zpd/ocr4all/application/spi/TrainingServiceProvider.java
@@ -0,0 +1,21 @@
+/**
+ * File:     TrainingServiceProvider.java
+ * Package:  de.uniwuerzburg.zpd.ocr4all.application.spi
+ * 
+ * Author:   Herbert Baier (herbert.baier@uni-wuerzburg.de)
+ * Date:     20.06.2024
+ */
+package de.uniwuerzburg.zpd.ocr4all.application.spi;
+
+import de.uniwuerzburg.zpd.ocr4all.application.spi.core.ActionServiceProvider;
+
+/**
+ * Defines service provider interfaces for training.
+ *
+ * @author <a href="mailto:herbert.baier@uni-wuerzburg.de">Herbert Baier</a>
+ * @version 1.0
+ * @since 1.8
+ */
+public interface TrainingServiceProvider extends ActionServiceProvider {
+
+}
